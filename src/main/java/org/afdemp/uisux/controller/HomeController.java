12567fe077ed7654fe4e5ad1a0319a44719e6cad
padoura <padoura@users.noreply.github.com>
2018-02-03 16:50:40 +0200

    Added controller for Products (methods productShelf and productDetail)

--- src/main/java/org/afdemp/uisux/controller/HomeController.java
+++ src/main/java/org/afdemp/uisux/controller/HomeController.java
@@ -1,11 +1,30 @@
 package org.afdemp.uisux.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.afdemp.uisux.domain.User;
+import org.afdemp.uisux.domain.Category;
+import org.afdemp.uisux.domain.Product;
+import org.afdemp.uisux.service.CategoryService;
+import org.afdemp.uisux.service.ProductService;
+import org.afdemp.uisux.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private CategoryService categoryService;
 
 	@RequestMapping("/")
 	public String index(){
@@ -27,47 +46,11 @@ public class HomeController {
 		model.addAttribute("classActiveLogin", true);
 		return "myAccount";
 	}
+	
+	
+	
 }
-//	
-//	
 
-//	
-//	@RequestMapping("/bookshelf")
-//	public String bookshelf(Model model, Principal principal) {
-//		if(principal != null) {
-//			String username = principal.getName();
-//			User user = userService.findByUsername(username);
-//			model.addAttribute("user", user);
-//		}
-//		
-//		List<Book> bookList = bookService.findAll();
-//		model.addAttribute("bookList", bookList);
-//		model.addAttribute("activeAll",true);
-//		
-//		return "bookshelf";
-//	}
-//	
-//	@RequestMapping("/bookDetail")
-//	public String bookDetail(
-//			@PathParam("id") Long id, Model model, Principal principal
-//			) {
-//		if(principal != null) {
-//			String username = principal.getName();
-//			User user = userService.findByUsername(username);
-//			model.addAttribute("user", user);
-//		}
-//		
-//		Book book = bookService.findOne(id);
-//		
-//		model.addAttribute("book", book);
-//		
-//		List<Integer> qtyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-//		
-//		model.addAttribute("qtyList", qtyList);
-//		model.addAttribute("qty", 1);
-//		
-//		return "bookDetail";
-//	}
 //
 //	@RequestMapping("/forgetPassword")
 //	public String forgetPassword(
---
+++ src/main/java/org/afdemp/uisux/controller/ProductController.java
@@ -0,0 +1,85 @@
+package org.afdemp.uisux.controller;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.afdemp.uisux.domain.Category;
+import org.afdemp.uisux.domain.Product;
+import org.afdemp.uisux.domain.User;
+import org.afdemp.uisux.service.CategoryService;
+import org.afdemp.uisux.service.ProductService;
+import org.afdemp.uisux.service.UserService;
+import org.afdemp.uisux.utility.ImageUtility;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@RequestMapping("/productShelf")
+	public String productShelf(Model model, Principal principal) {
+		if(principal != null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
+		List<Product> productList = productService.findAll();
+		List<Category> categoryList = categoryService.findAll();
+		
+		model.addAttribute("productList", productList);
+		model.addAttribute("activeAll",true);
+		model.addAttribute("categoryList", categoryList);
+		
+		return "productShelf";
+	}
+	
+	@RequestMapping("/productDetail")
+	public String productDetail(
+			@PathParam("id") Long id, Model model, Principal principal
+			) {
+		if(principal != null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
+		Product product = productService.findOne(id);
+		
+		model.addAttribute("product", product);
+		
+		List<Integer> qtyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		model.addAttribute("qtyList", qtyList);
+		model.addAttribute("qty", 1);
+		
+		return "productDetail";
+	}
+	
+	
+	
+	
+	
+
+
+}
